4988076a7b64283487d3965041bc421c3d437d12
blixa-rd <16521074@mahasiswa.itb.ac.id>
2023-04-11 22:26:10 +0700

    feat: ucs algorithm

---
+++ src/ucs.py
@@ -0,0 +1,44 @@
+# Uniform-Cost-Search (UCS) algorithm
+class UCS:
+    def __init__(self, graph, start, goal):
+        self.graph = graph
+        self.start = start
+        self.goal = goal
+        self.explored = {self.start: None} # dictionary for explored node
+        self.totalCost = {self.start: 0} # dictionary for the total cost of explored node
+        self.toVisit = PriorityQueue()
+    
+    def solve(self):
+        # cost without heuristic
+        self.start.cost = 0
+        self.toVisit.enqueue(self.start)
+        while not self.toVisit.isEmpty():
+            current = self.toVisit.dequeue()
+            if current == self.goal:
+                break
+            for neighbor in current.neighbors:
+                newCost = self.totalCost[current] + self.graph[current][neighbor]
+                # update the node with lowest cost
+                if neighbor not in self.totalCost or newCost < self.totalCost[neighbor]:
+                    self.totalCost[neighbor] = newCost
+                    neighbor.cost = newCost
+                    self.toVisit.enqueue(neighbor)
+                    self.explored[neighbor] = current
+        
+        # if goal node is not found, return empty
+        if (self.goal not in self.explored):
+            self.explored = {}
+        
+        # return node and total cost
+        return self.explored, self.totalCost
+    
+    def getPath(self):
+        current = self.goal
+        path = [current]
+        while current != self.start:
+            current = self.explored[current]
+            path.append(current)
+        
+        # get the path from start to goal
+        path.reverse()
+        return path
